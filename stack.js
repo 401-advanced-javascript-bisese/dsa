4d376e317035ae59ff98bc647398b8b1aec2a515
scottbisese <bisese.scott@gmail.com>
2019-09-27 16:25:02 -0500

    stackin big time

---
+++ stack.js
@@ -0,0 +1,15 @@
+class Stack {
+  constructor() {
+    this.head === null;
+  }
+  push(value) {
+    let nudeNode = {  //nudeNode is really newNode
+      value: value,
+      next: null,
+    };
+    if (!this.head) {
+      this.head === nudeNode;
+      return;
+    }
+  }
+}
